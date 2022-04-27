ea2d6f1b9c42ee953af2a22659a747f65f9f16fa
Ahonoryoshi <yoshiahona@gmail.com>
2022-04-27 08:15:43 +0300

    add instance variables to class User

--- user.py
+++ user.py
@@ -2,4 +2,19 @@ class User:
     '''
     Class that generates new instances of users
     '''
-    pass
+    def __init__(self,first_name,last_name,phone_number,email):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New User first name.
+            last_name : New User last name.
+            number: New contact phone number.
+            email : New contact email address.
+        '''
+        self.first = first_name
+        self.last = last_name
+        self.number = phone_number
+        self.email = email
+       
